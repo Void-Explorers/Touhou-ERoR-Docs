afec1e49e19e5c6a34ec66a405d856ceea878fa2
Dongxin Tian <70252050+kakitsubata7@users.noreply.github.com>
2025-01-12 14:20:00 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Void-Explorers', // Usually your GitHub org/user name.
   projectName: 'Touhou-Shinshuuyume-Page', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
